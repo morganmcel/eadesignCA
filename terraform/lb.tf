d2405d9476f7a100ba220221d3e8b1563d296db5
Morgan McElligott <morgan@mmce.me>
2022-05-03 20:15:38 +0100

    Update to LB SG

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "alb" {
   internal           = local.lb["internal"]
   load_balancer_type = "application"
   subnets            = [for s in data.aws_subnet.subnets : s.id]
+  security_groups    = [aws_security_group.allow_http.id]
 }
 
 resource "aws_lb_target_group" "group" {
@@ -23,5 +24,31 @@ resource "aws_lb_listener" "front_end" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.group.arn
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP from Anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
   }
 }
